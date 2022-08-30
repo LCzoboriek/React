0759755f0270f8192630c2a0b8b191d510b830c9
Luke Czoboriek <lc169782@protonmail.com>
2022-08-30 12:45:51 +0100

    Created an array, and used the map functions to render the array out on the screen

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ function getTitle(title) {
   return title;
 }
 
+let my_array = [123, "string"];
+
 function App() {
   return (
     <div>
+      <h1>{my_array.map((x) => x)}</h1>
       <h1>Hello {getTitle("React")}</h1>
       <label htmlFor="search">Search:</label>
       <label id="search" type="text" />
