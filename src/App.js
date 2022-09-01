9bb571cb3e44a2e3eb0e1b719925ff830441f4f8
Luke Czoboriek <lc169782@protonmail.com>
2022-09-01 15:18:32 +0100

    moved list component from the global scope to the app function

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: "MDN",
-    url: "https://developer.mozilla.org",
-    author: "Mozilla",
-    num_comments: 5,
-    points: 3,
-    objectID: 2,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: "MDN",
+      url: "https://developer.mozilla.org",
+      author: "Mozilla",
+      num_comments: 5,
+      points: 3,
+      objectID: 2,
+    },
+  ];
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
-    <Search />
+      <hr />
 
-    <hr />
-
-    <List />
-    <List />
-    <List />
-  </div>
-);
+      <List />
+      <List />
+      <List />
+    </div>
+  );
+};
 
 const List = () => (
   <ul>
