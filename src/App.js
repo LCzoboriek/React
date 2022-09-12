4632a9070cc6ecf2984d01327e9fe74d4d7ba366
Luke Czoboriek <lc169782@protonmail.com>
2022-09-12 14:54:51 +0100

    Have destructured the objects for props so it makes the components more functionally readable and consise, this is a refactoring step, not something you begin with!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import * as React from "react";
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = React.useState(""); // Remember that in the useState function, the first const is the current state, and the second part is the state to be updating the current state. So in this case, whatever
-  //is typed is set to setSearchTerm, which then updates searchTerm
+  const [searchTerm, setSearchTerm] = React.useState("React"); //This is called controller components within react, this sets the default state to being something specific
+  //we do this as it enforces a predictable behaviour.
   const stories = [
     {
       title: "React",
@@ -23,62 +23,53 @@ const App = () => {
   ];
 
   const handleSearch = (event) => {
-    //We are now instantiating the searchTerms state here, as we want to filter the stories within app,
-    //which is then passed up via a callback handler, from the search component to the app component (child to parent)
     setSearchTerm(event.target.value);
     console.log(event.target.value);
   };
 
   const searchedStories = stories.filter(function (story) {
     return story.title.toLowerCase().includes(searchTerm.toLowerCase());
-  }); // We then use the filter function, which takes the argument of story as its parameter, and returns itself if the attribute of title, includes the updated state from above
+  });
   console.log("App renders");
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} />
-      {/* Here is where the props is set  */}
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
       <List list={searchedStories} />
-      {/* We then update the html/jsx entry to now display only the searched stories, which is set via the above methods/functions */}
     </div>
   );
 };
 
-const Search = (props) => (
+const Search = (
+  { search, onSearch } //Here we have desrctured the props object right away in the function signature of the component which enabled us to omite the functions block body of the component still
+) => (
   <div>
     <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onChange={props.onSearch} />
-    {/* Call back handler here, this is done via the onChange state handler, which passes up the props from the part on line 34 */}
+    <input id="search" type="text" value={search} onChange={onSearch} />
   </div>
 );
 
-const List = (props) => {
-  console.log("list renders");
-  return (
-    <ul>
-      {props.list.map((item) => (
-        <Item key={item.objectID} item={item} />
-      ))}
-    </ul>
-  );
-};
+const List = ({ list }) => (
+  <ul>
+    {list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
 
-const Item = (props) => {
-  console.log("Item renders");
-  return (
-    <li>
-      <span>
-        <a href={props.item.url}>{props.item.title}</a>
-      </span>
-      <span>{props.item.author}</span>
-      <span>{props.item.num_comments}</span>
-      <span>{props.item.points}</span>
-    </li>
-  );
-};
+const Item = ({ item }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+  </li>
+);
 
 export default App;
