4b6cc6ce7873d595ec682f39cd1b3b356b767c25
Luke Czoboriek <lc169782@protonmail.com>
2022-10-31 15:37:19 +0000

    Added in remote api connection to pull across data

--- src/App.js
+++ src/App.js
@@ -1,127 +1,109 @@
 import * as React from "react";
-import { v4 as uuidv4 } from "uuid";
+
+const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?query=";
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case "STORIES_FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "STORIES_FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "STORIES_FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case "REMOVE_STORY":
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
+};
 
 const useStorageState = (key, initialState) => {
   const [value, setValue] = React.useState(
-    localStorage.getItem(key) ?? initialState
+    localStorage.getItem(key) || initialState
   );
 
   React.useEffect(() => {
     localStorage.setItem(key, value);
-  }, [value, key]); // When the dependency array attribute/variable changes, it will trigger the side effect localStorage.setItem("search", searchTerm)
-  //It works really similarly to a useState, the second part in the arguments is the newly updated variable, and the search is the current state, which gets
-  //updated
+  }, [value, key]);
 
   return [value, setValue];
 };
 
 const App = () => {
-  // const [searchTerm, setSearchTerm] = React.useState(
-  //   localStorage.getItem("search") ?? "React" //Doing this will sync the browsers storage with reacts state, if the storage exists,
-  //   //itll display that, if not, itll default to React, this is a shorthand truthy operation, this however introduces
-  //   //a side effect, this is when the React.useEffect hook comines into play to trigger the wanted side-effect, each time the searchTerm changes
-  // );
-
-  // React.useEffect(() => {
-  //   localStorage.setItem("search", searchTerm);
-  // }, [searchTerm]);
-
-  const initialStories = [
-    {
-      title: "React",
-      url: "https://reactjs.org/",
-      author: "Jordan Walke",
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: "Redux",
-      url: "https://redux.js.org/",
-      author: "Dan Abramov, Andrew Clark",
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
-
-  const getAsyncStories = () =>
-    new Promise((resolve) =>
-      setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
-    );
-
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
 
-  const [stories, setStories] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(false);
-  const [isError, setIsError] = React.useState(false);
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
 
   React.useEffect(() => {
-    setIsLoading(true);
-    getAsyncStories()
+    dispatchStories({ type: "STORIES_FETCH_INIT" });
+
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
-        setStories(result.data.stories);
-        setIsLoading(false);
+        dispatchStories({
+          type: "STORIES_FETCH_SUCCESS",
+          payload: result.hits,
+        });
       })
-      .catch(() => setIsError(true));
+      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
   }, []);
 
-  //This above makes the list stateful, making it dynamic and to be added to
-  //We also need to make the input field for things to be added dynamic as well
-  const [name, setName] = React.useState("");
-  //We then add an event handler for the input field along with an attribute for calling the function
-  function handleChange(event) {
-    setName(event.target.value);
-  }
-
-  function handleAdd() {
-    const newList = stories.concat({ name, id: uuidv4() });
-    setStories(newList);
-    setName("");
-  }
-
   const handleRemoveStory = (item) => {
-    const newStories = stories.filter(
-      (story) => item.objectID !== story.objectID
-    );
-
-    setStories(newStories);
+    dispatchStories({
+      type: "REMOVE_STORY",
+      payload: item,
+    });
   };
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  const searchedStories = stories.filter(function (story) {
-    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
-  });
-  console.log("App renders");
+  const searchedStories = stories.data.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <div></div>
+
       <InputWithLabel
         id="search"
-        label="Search"
         value={searchTerm}
         isFocused
         onInputChange={handleSearch}
       >
-        <strong>Search here:</strong>
+        <strong>Search:</strong>
       </InputWithLabel>
-      <br />
-      <br />
-      <strong>Enter book to add:</strong>
-      <input type="text" value={name} onChange={handleChange} />
-      {/* Heres where we have added the event handler tag with passing up the name attribute */}
-      <button type="button" onClick={handleAdd}>
-        Add
-      </button>
 
       <hr />
-      {isError && <p>Something went wrong...</p>}
-      {isLoading ? (
-        <p>Loading...</p>
+
+      {stories.isError && <p>Something went wrong ...</p>}
+
+      {stories.isLoading ? (
+        <p>Loading ...</p>
       ) : (
         <List list={searchedStories} onRemoveItem={handleRemoveStory} />
       )}
@@ -144,6 +126,7 @@ const InputWithLabel = ({
       inputRef.current.focus();
     }
   }, [isFocused]);
+
   return (
     <>
       <label htmlFor={id}>{children}</label>
@@ -154,7 +137,6 @@ const InputWithLabel = ({
         type={type}
         value={value}
         onChange={onInputChange}
-        autoFocus={isFocused}
       />
     </>
   );
@@ -168,28 +150,20 @@ const List = ({ list, onRemoveItem }) => (
   </ul>
 );
 
-const Item = ({ item, onRemoveItem }) => {
-  const handleRemoveItem = () => {
-    onRemoveItem(item);
-  };
-  return (
-    <li>
-      <span>
-        <a href={item.url}>{item.title}</a>
-      </span>
-      <br />
-      <span> Author/s: {item.author}</span>
-      <br />
-      <span>Number of comments: {item.num_comments}</span>
-      <br />
-      <span>Rating: {item.points} </span>
-      <br />
-      <span>
-        <button type="button" onClick={handleRemoveItem}>
-          Dismiss
-        </button>
-      </span>
-    </li>
-  );
-};
+const Item = ({ item, onRemoveItem }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
+    <span>
+      <button type="button" onClick={() => onRemoveItem(item)}>
+        Dismiss
+      </button>
+    </span>
+  </li>
+);
+
 export default App;
